819b59dff3fcfb1e7eadb984c0198f8df303578b
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-10-15 18:03:45 +0530

    Added:Introducing Service Layer In Emp Payroll App

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeePayrollService.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.employeepayrollapp.service;
+
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
+
+import java.util.List;
+
+public class EmployeePayrollService implements IEmployeePayrollService{
+
+    @Override
+    public List<EmployeePayrollData> getEmployeePayrollData() {
+        return null;
+    }
+
+    @Override
+    public EmployeePayrollData getEmployeePayrollDataById(int empId) {
+        return null;
+    }
+
+    @Override
+    public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) {
+        return null;
+    }
+
+    @Override
+    public EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO employeePayrollDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmployeePayrollData(int empId) {
+
+    }
+}
