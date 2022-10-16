0c557b25fa87dd54e3ba2594c6b010b0c8f1d76c
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-10-16 11:23:07 +0530

    Added:Lombok_Library

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/ResponseDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/ResponseDTO.java
@@ -1,13 +1,13 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class ResponseDTO {
+public @Data class ResponseDTO {
     private  String message;
-
     private Object data;
 
     public ResponseDTO(String message,Object data){
--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -5,15 +5,18 @@ package com.bridgelabz.employeepayrollapp.model;
 
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+/***********************************************************************************************************************
+ * Class :- Employee Payroll Data.
+ * @author Himanshu
+ **********************************************************************************************************************/
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 
-@Getter
-@Setter
+
+@Data
 @Entity
 public class EmployeePayrollData {
     @Id
